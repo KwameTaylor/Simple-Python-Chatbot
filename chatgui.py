d8ebc18d2eb4da7f07320b83c40cc31cf43ba17d
Kwame Taylor <maxkwametaylor@gmail.com>
2020-09-30 15:26:21 -0500

    stopping point - learn nltk

--- chatgui.py
+++ chatgui.py
@@ -39,7 +39,7 @@ def predict_class(sentence, model):
     # filter out predictions below a threshold
     p = bow(sentence, words,show_details=False)
     res = model.predict(np.array([p]))[0]
-    ERROR_THRESHOLD = 0.25
+    ERROR_THRESHOLD = 0.13
     results = [[i,r] for i,r in enumerate(res) if r>ERROR_THRESHOLD]
     # sort by strength of probability
     results.sort(key=lambda x: x[1], reverse=True)
@@ -75,10 +75,10 @@ def send():
     if msg != '':
         ChatLog.config(state=NORMAL)
         ChatLog.insert(END, "You: " + msg + '\n\n')
-        ChatLog.config(foreground="#442265", font=("Verdana", 12 ))
+        ChatLog.config(foreground="#442265", font=("Georgia", 12 ))
 
         res = chatbot_response(msg)
-        ChatLog.insert(END, "Bot: " + res + '\n\n')
+        ChatLog.insert(END, "Koneko: " + res + '\n\n')
 
         ChatLog.config(state=DISABLED)
         ChatLog.yview(END)
@@ -87,10 +87,10 @@ def send():
 base = Tk()
 base.title("Hello")
 base.geometry("400x500")
-base.resizable(width=FALSE, height=FALSE)
+base.resizable(width=FALSE, height=TRUE)
 
 #Create Chat window
-ChatLog = Text(base, bd=0, bg="white", height="8", width="50", font="Arial",)
+ChatLog = Text(base, bd=0, bg="white", height="8", width="50", font="Georgia",)
 
 ChatLog.config(state=DISABLED)
 
@@ -99,12 +99,12 @@ scrollbar = Scrollbar(base, command=ChatLog.yview, cursor="heart")
 ChatLog['yscrollcommand'] = scrollbar.set
 
 #Create Button to send message
-SendButton = Button(base, font=("Verdana",12,'bold'), text="Send", width="12", height=5,
-                    bd=0, bg="#32de97", activebackground="#3c9d9b",fg='#ffffff',
+SendButton = Button(base, font=("Georgia",14,'bold'), text="SEND", width="12", height=6,
+                    bd=0, bg="#32de97", activebackground="#3c9d9b",fg='#45baa5',
                     command= send )
 
 #Create the box to enter message
-EntryBox = Text(base, bd=0, bg="white",width="29", height="5", font="Arial")
+EntryBox = Text(base, bd=0, bg="lavender",width="29", height="5", font="Georgia")
 #EntryBox.bind("<Return>", send)
 
 
